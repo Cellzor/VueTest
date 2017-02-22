3e4e924a52950a6dabac376b5b702a5bab6f313c
Christian Kuhr <Christian@noplay.com>
2017-02-22 22:14:29 +0100

    Added lists and arrayMutation

--- js/app.js
+++ js/app.js
@@ -213,4 +213,29 @@ var watchExample = new Vue({
         }
     });
 
-
+Vue.component('todo-item', {
+    template: '\
+    <li>\
+      {{ title  }}\
+      <button v-on:click="$emit(\'remove\')">X</button>\
+    </li>\
+  ',
+    props: ['title']
+});
+var todoExample = new Vue({
+    el: '#todo-list-example',
+    data: {
+        newTodoText: '',
+        todos: [
+            'Do the dishes',
+            'Take out the trash',
+            'Mow the lawn'
+        ]
+    },
+    methods: {
+        addNewTodo: function () {
+            this.todos.push(this.newTodoText)
+            this.newTodoText = ''
+        }
+    }
+});
