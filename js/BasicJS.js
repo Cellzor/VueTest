c904a78ebe8a0c09386f5f2aa2e9e4b680980d8c
Christian Kuhr <Christian@noplay.com>
2017-02-26 22:14:28 +0100

    added more basic Js stuff

--- js/BasicJS.js
+++ js/BasicJS.js
@@ -4,12 +4,63 @@
 
 
 // **************** Basic functions ***************************************************************
-var myNumber = '5'; //Basic variable
+    //6 basic types of values
+var myNumber = 5;                       //Number n^64, 18 zeroes
+// Special cases: Infinity, -Infinity & NaN (Not a number) are all valid integers
+// NaN == NaN > false, use isNaN(myNumber)
+var myDecimal = 1.5;                    //15 zeroes, i.e. not precis valuesll
+var myString = 'Hello World!';          //String
+console.log(typeof myDecimal);
+console.log(typeof myString);
+
+var myBool = true;                      //Boolean
+var myObject = {                        //Object
+    name : "Triangle",
+    Lenght : 10,
+    Hello : function () {
+        alert("Hello World!")
+
+    }
+};
+var myFunction = function () {     //Function
+    return true;
+
+};
+var undefined;                          //Undefined Value
+var myNull = null;                      //Null
 var myArray = ['red', 'blue', 'green'];   //Basic array
+
+//ternary operator = operates on 3 values, e.g.
+//      true ? 1 : 2    //  > 1
+//      false ? 1 : 2   //  > 2
+//Binary operator =  operates on 2 values, e.g. + - /*
+//Unary operator = operates on 1 value, - typeof
+
+// == and != do cause auto conversion meaning 0 == false and "" == false return true
+// === and !== do not auto convert, meaning 0 === false and "" !== false return false
+
+// Highest prior > lowest Prio
+//  */ +- == && ||
+//1 + 1 == 2 && 10 * 10 > 50
+
+//The || operator, will return the value to its left when that can be converted to true
+// and will return the value on its right otherwise
+console.log(null || "user")
+// → user
+console.log("Karl" || "user")
+// → Karl
+
+//The && operator works similarly, but the other way around.
+// When the value to its left // is something that converts to false,
+// it returns that value, and otherwise it returns the value on its right.
+
 
 myArray.push('purple');
 console.log(myNumber);
 console.log(myArray.reverse());
+
+// Break breaks the loop entirely
+// Continue stops the current iteration and continues with the next
 
 
 for(var i=0; i <3; i++){
@@ -25,6 +76,30 @@ while(j<3){
 myArray.forEach(function(i){
     console.log("myArray: "+i);
 } );
+
+var doSwitch  =function () {
+    switch (prompt("What is the weather like?")) {
+        case "rainy":
+            console.log("Remember to bring an umbrella.");
+            break;
+        case "sunny":
+            console.log("Dress lightly.");  //No Break, execution continues until break
+        case "cloudy":
+            console.log("Go outside.");
+            break;
+        default:                            //Default will run if no case matches
+            console.log("Unknown weather type!");
+            break;
+    }
+};
+
+var doLoop = function () {
+    do {
+        var yourName = prompt("Who are you?");
+    } while (!yourName);
+    console.log(yourName);
+};
+
 
 // **************** Objects ***********************************************************************
 
@@ -131,5 +206,48 @@ function validateForm(){
     } else if (firstName.length < 3){
         alert("First name must have atleast 3 characters");
         return false;
+    }
+}
+function doPrompt(){
+    prompt("Tell me everything you know");
+};
+function doConfirm() {
+    confirm("Shall we, then?");
+};
+function createTriangle(){
+    triangle = "#";
+    for(var i = 0; i<7; i++){
+        console.log(triangle);
+        triangle += "#";
+    }
+};
+function fizzBuzz(){
+    var counter = 0;
+    while(counter < 100) {
+        counter++;
+        if (counter === 3)
+            console.log("Fizz");
+        else if (counter % 5 === 0)
+            console.log("Buzz");
+        else
+            console.log(counter);
+    }
+};
+function createChessBoard(){
+    size = 8;
+    var board = "";
+    for(var j=0; j<size/2; j++){
+        for(var i=0; i<size*2; i++){
+            if(i === size){
+                board += "\n";
+            }
+            else if(i%2 === 0){
+                board += " ";
+            } else {
+                board += "#";
+            }
+        }
+        console.log(board);
+        board = "";
     }
 }
---
+++ js/test.ts
@@ -0,0 +1,23 @@
+
+/**
+ * Created by Christian on 2017-02-22.
+ */
+function Greeter(greeting: string) {
+    this.greeting = greeting;
+}
+
+Greeter.prototype.greet = function() {
+    return "Hello, " + this.greeting;
+}
+
+// Oops, we're passing an object when we want a string. This will print
+// "Hello, [object Object]" instead of "Hello, world" without error.
+let greeter = new Greeter("world");
+
+let button = document.createElement('button');
+button.textContent = "Say Hello";
+button.onclick = function() {
+    alert(greeter.greet());
+};
+
+document.body.appendChild(button);
